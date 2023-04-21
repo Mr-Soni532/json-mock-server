7d61ce15bed31890ee2492ec47e8350cd3b4c841
Bhupender Soni <official.soni532@gmail.com>
2023-04-21 11:19:27 +0530

    rem

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-// server.js
 const jsonServer = require('json-server')
+// const cors=require("cors")
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-require('dotenv')
-
+// server.use(cors)
 server.use(middlewares)
 server.use(router)
-server.use(jsonServer.bodyParser)
-
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
-})
+});
