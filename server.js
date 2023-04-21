d9731c9db77cfb8e14171802e7e18eaa8b0ecede
Bhupender Soni <official.soni532@gmail.com>
2023-04-21 11:31:04 +0530

    given write permission

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const jsonServer = require('json-server')
+const fs = require('fs');
 // const cors=require("cors")
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 // server.use(cors)
+fs.chmodSync('db.json', '666')
 server.use(middlewares)
+server.use(jsonServer.bodyParser)
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
