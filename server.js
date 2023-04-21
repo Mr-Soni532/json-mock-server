d3dae7108e6d06de461811c4fd0bd35dd8b600b5
Bhupender Soni <official.soni532@gmail.com>
2023-04-21 11:34:21 +0530

    removed  write permission

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const jsonServer = require('json-server')
-const fs = require('fs');
+// const fs = require('fs');
 // const cors=require("cors")
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 // server.use(cors)
-fs.chmodSync('db.json', '666')
+// fs.chmodSync('db.json', '666')
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
 server.use(router)
